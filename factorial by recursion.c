40fb72f356eac125ac7c7ea328960dab0bc78fef
Shambhavicoder <shambhavi.-cse2024@indoreinstitute.com>
2025-09-02 11:08:32 +0530

    Add files via upload

---
+++ factorial by recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int factorial(int a);
+int main(){
+	int a;
+	printf("Enter a number");
+	scanf("%d",&a);
+	printf(factorial(a));
+	}
+int factorial(int a){
+	if(a == 0){
+		return 1;
+	}
+	else{
+		return a* factorial(a-1);
+	}
+}
+   
+	
+
