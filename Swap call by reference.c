59ec05ce089934c5a15eb42bbb01068488bb09f5
Shambhavicoder <shambhavi.-cse2024@indoreinstitute.com>
2025-09-16 10:46:37 +0530

    Add files via upload

---
+++ Swap call by reference.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+swap(int *a,int *b){
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+	return *a;
+	return *b;
+   // printf("a=%d and b=%d",a,b);
+	
+}
+int main(){
+	int a,b;
+	printf("Enter first value a= ");
+	scanf("%d",&a);
+	printf("Enter second value b= ");
+	scanf("%d",&b);
+	printf("a=%d and b=%d",swap(&a,&b));
+
+}
